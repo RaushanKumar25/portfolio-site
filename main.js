9993eeb1f294249ea923d8cf371f3ca4c387df1f
RaushanKumar25 <raushankumarbhardwaj4510@gmail.com>
2025-08-15 16:55:27 +0530

    created  my skillssection and added icons to all skills with vibrant color

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer.", "Web Developer.", "Cloud Enthusiast.", "Open Source Contributor.", "Problem Solver"],
+    strings: ["Frontend Developer.", "Web Developer.", "Cloud Enthusiast.", "Open Source Contributor.", "Problem Solver."],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
